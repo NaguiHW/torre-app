05f7bc078b3ba060a36c00d2eab21df0555a7b90
Kalib Hackin <nagui08@hotmail.com>
2020-11-09 13:43:13 -0500

    html part is done

---
+++ src/containers/Home/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './style.scss';
+
+const Home = () => (
+  <div className="home">
+    <div className="hero1">
+      <div className="logo">
+        <h1>
+          <span>T</span>orre<span>S</span>earch
+        </h1>
+      </div>
+      <div className="content">
+        <h1>
+          Are you a <span>professional</span> or a <span>freelancer</span> looking for a job opportunity?
+          <br />
+          You are in the right place. Start looking <span>now</span>.
+        </h1>
+        <button type="button">Search Opportunities</button>
+      </div>
+    </div>
+    <div className="hero2">
+      <h1>
+        Are you looking for a <span>qualified professional</span>? Or a <span>freelancer</span>?
+        <br />
+        You are in the right place. Start looking <span>now</span>.
+      </h1>
+      <button type="button">Search Professionals</button>
+    </div>
+  </div>
+);
+
+export default Home;
