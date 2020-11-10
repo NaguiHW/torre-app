cd14ce7141445fedc0f916155f59c97fbe9d3a04
Kalib Hackin <nagui08@hotmail.com>
2020-11-10 00:00:07 -0500

    styled individual job containers

--- src/components/Job/index.jsx
+++ src/components/Job/index.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import './style.scss';
 
-const Job = () => (
+const Job = ({
+  title,
+  imgSrc,
+  organizationName,
+  skills,
+  location,
+  type,
+  deadline,
+  compensation,
+}) => (
   <div className="job">
-    <div className="title">
-      
+    <div className="title" style={{backgroundImage: `url(${imgSrc})`}} />
+    <div className="info">
+      <h1>{title}</h1>
+      <h2 className="organization-name">{organizationName}</h2>
+      <p>Location: <b>{location ? location : '-'}</b></p>
+      <p>Type: <b>{type ? type : '-'}</b></p>
+      <p>Deadline: <b>{deadline ? deadline.split('T', 1) : '-'}</b></p>
+      <p>Min: <b>{compensation?.data ? compensation.data.minAmount : '-'}</b></p>
+      <p>Max: <b>{compensation?.data ? compensation.data.maxAmount : '-'}</b></p>
+      <p>Currency: <b>{compensation?.data ? compensation.data.currency : '-'}</b></p>
+      <p>Periodicity: <b>{compensation?.data ? compensation.data.periodicity : '-'}</b></p>
+      <h3>Skills:</h3>
+      <div className="skills-container">
+        {
+          skills?.map(skill => (
+            <h5>{skill.name}</h5>
+          ))
+        }
+      </div>
     </div>
   </div>
-)
+);
+
+export default Job;
--- src/containers/Jobs/index.jsx
+++ src/containers/Jobs/index.jsx
@@ -1,17 +1,65 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import Job from '../../components/Job';
 import Navbar from '../../components/Navbar';
 import './style.scss';
 
-const Jobs = () => (
-  <div className="jobs">
-    <div className="hero">
-      <Navbar />
-      <h1>Job Opportunity</h1>
-    </div>
-    <div className="jobs-container">
+const Jobs = () => {
+  const [state, setState] = useState({
+    page: 1,
+    size: 20,
+    offset: 0,
+    jobs: [],
+  });
 
+  useEffect(() => {
+    const req = async () => {
+
+      const response = await axios.post(`https://search.torre.co/opportunities/_search/?size=${state.size}&offset=${state.offset}`);
+      setState({
+        ...state,
+        jobs: response.data.results.map(job => ({
+          id: job.id,
+          title: job.objective,
+          organization: job.organizations,
+          skills: job.skills,
+          type: job.type,
+          status: job.status,
+          deadline: job.deadline,
+          locations: job.locations,
+          compensation: job.compensation,
+        }))
+      })
+      console.log(response.data.results)
+    }
+
+    req();
+  }, [state.size, state.offset])
+
+  return (
+    <div className="jobs">
+      <div className="hero">
+        <Navbar />
+        <h1>Job Opportunity</h1>
+      </div>
+      <div className="jobs-container">
+        {
+          state.jobs?.map(job => (
+            <Job
+              title={job.title}
+              imgSrc={job.organization[0].picture}
+              organizationName={job.organization[0].name}
+              skills={job.skills}
+              location={job.locations[0]}
+              type={job.type}
+              deadline={job.deadline}
+              compensation={job.compensation}
+            />
+          ))
+        }
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Jobs;
