d2cc47383ba8221e7f16d5268711badc807bfce4
Kalib Hackin <nagui08@hotmail.com>
2020-11-10 01:29:52 -0500

    changing between pages is complete

--- src/containers/Jobs/index.jsx
+++ src/containers/Jobs/index.jsx
@@ -6,16 +6,62 @@ import './style.scss';
 
 const Jobs = () => {
   const [state, setState] = useState({
-    page: 1,
+    actualPage: 1,
     size: 20,
-    offset: 0,
     jobs: [],
+    total: 0,
   });
+
+  const pages = () => {
+    const { actualPage, size, total } = state;
+    const lastPage = total%size === 0 ? total/size : Math.ceil(total/size);
+    const menuLength = 7;
+    
+    if (actualPage < 5) {
+      return lastPage < menuLength
+        ? Array.from({length: lastPage}, (x, i) => i + 1)
+        : Array.from({length: menuLength}, (x, i) => i + 1)
+    }
+    if (actualPage > lastPage - 4) {
+      return lastPage < menuLength
+        ? Array.from({length: lastPage}, (x, i) => i + 1)
+        : Array.from({length: menuLength}, (x, i) => lastPage - 6 + i)
+    }
+    if (actualPage > 4 && actualPage < lastPage - 3) {
+      return Array.from({length: menuLength}, (x, i) => actualPage - 3 + i)
+    }
+  }
+
+  const updatePage = e => {
+    setState({
+      ...state,
+      actualPage: e.target.innerHTML,
+    })
+  }
+
+  const goTo = e => {
+    const { size, total } = state;
+    const lastPage = total%size === 0 ? total/size : Math.ceil(total/size);
+    if (e.target.innerHTML === 'First') {
+      setState({
+        ...state,
+        actualPage: 1,
+      });
+    }
+    if (e.target.innerHTML === 'Last') {
+      setState({
+        ...state,
+        actualPage: lastPage,
+      });
+    }
+  }
 
   useEffect(() => {
     const req = async () => {
+      const { actualPage, size } = state;
+      const offset = (actualPage - 1) * size
 
-      const response = await axios.post(`https://search.torre.co/opportunities/_search/?size=${state.size}&offset=${state.offset}`);
+      const response = await axios.post(`https://search.torre.co/opportunities/_search/?size=${size}&offset=${offset}`);
       setState({
         ...state,
         jobs: response.data.results.map(job => ({
@@ -28,13 +74,14 @@ const Jobs = () => {
           deadline: job.deadline,
           locations: job.locations,
           compensation: job.compensation,
-        }))
+        })),
+        total: response.data.total,
       })
-      console.log(response.data.results)
     }
 
     req();
-  }, [state.size, state.offset])
+    window.scrollTo(0, 0)
+  }, [state.size, state.actualPage])
 
   return (
     <div className="jobs">
@@ -42,13 +89,18 @@ const Jobs = () => {
         <Navbar />
         <h1>Job Opportunity</h1>
       </div>
+      <div className="drop-down">
+        <button type="button">20</button>
+        <button type="button">60</button>
+        <button type="button">100</button>
+      </div>
       <div className="jobs-container">
         {
           state.jobs?.map(job => (
             <Job
               title={job.title}
-              imgSrc={job.organization[0].picture}
-              organizationName={job.organization[0].name}
+              imgSrc={job.organization[0]?.picture}
+              organizationName={job.organization[0]?.name}
               skills={job.skills}
               location={job.locations[0]}
               type={job.type}
@@ -57,6 +109,15 @@ const Jobs = () => {
             />
           ))
         }
+      </div>
+      <div className="pages">
+        <span onClick={goTo}>First</span>
+        {
+          pages().map(page => (
+            <p onClick={updatePage}>{page}</p>
+          ))
+        }
+        <span onClick={goTo}>Last</span>
       </div>
     </div>
   );
