bfbecbbf823b8c3dc3ef2f75ffe8ec535e4fee3e
Kalib Hackin <naguihw@gmail.com>
2020-11-28 16:00:58 -0500

    installing fetch

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 const express = require('express');
 const cors = require('cors');
+const fetch = require('node-fetch');
 
 // API
 
@@ -17,6 +18,13 @@ app.get('/', (request, response) => response.status(200).send('Hello World'));
 app.post('/people', async (request, response) => {
   const size = request.query.size;
   const offset = request.query.offset;
+
+  const res = await fetch(`https://search.torre.co/people/_search/?size=${size}&offset=${offset}`, {
+    method: 'POST',
+  });
+
+  console.log(size, offset);
+  console.log(res);
 })
 
 // Listen Command
