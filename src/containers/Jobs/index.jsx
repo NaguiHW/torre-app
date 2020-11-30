2d33119ac0084addb856b8bd9fae1044b6e9bb23
Kalib Hackin <naguihw@gmail.com>
2020-11-30 13:51:08 -0500

    fixed next and prev page in job opportunity

--- src/containers/Jobs/index.jsx
+++ src/containers/Jobs/index.jsx
@@ -18,48 +18,25 @@ const Jobs = () => {
     button: "display"
   });
 
-  const pages = () => {
-    const { actualPage, size, total } = state;
-    const lastPage = total % size === 0 ? total / size : Math.ceil(total / size);
-    const menuLength = 7;
-
-    if (actualPage < 5) {
-      return lastPage < menuLength
-        ? Array.from({ length: lastPage }, (x, i) => i + 1)
-        : Array.from({ length: menuLength }, (x, i) => i + 1)
-    }
-    if (actualPage > lastPage - 4) {
-      return lastPage < menuLength
-        ? Array.from({ length: lastPage }, (x, i) => i + 1)
-        : Array.from({ length: menuLength }, (x, i) => lastPage - 6 + i)
-    }
-    if (actualPage > 4 && actualPage < lastPage - 3) {
-      return Array.from({ length: menuLength }, (x, i) => actualPage - 3 + i)
-    }
-  }
-
-  const updatePage = e => {
-    setState({
-      ...state,
-      actualPage: e.target.innerHTML,
-    })
-  }
-
   const goTo = e => {
-    const { size, total } = state;
-    const lastPage = total % size === 0 ? total / size : Math.ceil(total / size);
-    if (e.target.innerHTML === 'First') {
+    let newPage = state.actualPage;
+    if (e.target.innerHTML === 'Prev') {
+      newPage--;
       setState({
         ...state,
-        actualPage: 1,
+        actualPage: newPage,
       });
     }
-    if (e.target.innerHTML === 'Last') {
+
+    if (e.target.innerHTML === 'Next') {
+      newPage++
       setState({
         ...state,
-        actualPage: lastPage,
+        actualPage: newPage,
       });
     }
+    console.log(state.actualPage);
+    console.log(state.actualPage === Math.ceil(state.total / state.size));
   }
 
   const updateSize = e => {
@@ -93,7 +70,7 @@ const Jobs = () => {
       radioButtons[i].checked = false;
     }
     delete formData[radioButtons[1].name]
-  }
+  };
 
   const handleChange = e => {
     const element = e.target;
@@ -112,75 +89,54 @@ const Jobs = () => {
         [element.name]: element.value,
       });
     }
-  }
+  };
 
   const clearText = e => {
     const textField = e.target.parentElement.children[2];
     textField.value = '';
-
+    console.log(state);
     delete formData[textField.name];
-  }
+  };
 
   const search = async e => {
-    e.preventDefault();
+    e && e.preventDefault();
     const { actualPage, size } = state;
     const offset = (actualPage - 1) * size;
     const hasData = Object.keys(formData).length > 0;
+    let response;
 
     if (hasData) {
-      const response = await axios({
+      response = await axios({
         method: 'post',
         url: `/opportunities/search?size=${size}&offset=${offset}`,
         data: formData,
       });
-
-      setState({
-        ...state,
-        jobs: response.data.data.map(job => ({
-          id: job.id,
-          title: job.objective,
-          organization: job.organizations,
-          skills: job.skills,
-          type: job.type,
-          status: job.status,
-          deadline: job.deadline,
-          locations: job.locations,
-          compensation: job.compensation,
-        })),
-        total: response.data.total,
-      });
-
-    }
-  }
-
-  useEffect(() => {
-    const req = async () => {
-      const { actualPage, size } = state;
-      const offset = (actualPage - 1) * size
-
-      const response = await axios({
+    } else {
+      response = await axios({
         method: 'post',
         url: `/opportunities/search?size=${size}&offset=${offset}`,
       });
-      console.log(response);
-      setState({
-        ...state,
-        jobs: response.data.data.map(job => ({
-          id: job.id,
-          title: job.objective,
-          organization: job.organizations,
-          skills: job.skills,
-          type: job.type,
-          status: job.status,
-          deadline: job.deadline,
-          locations: job.locations,
-          compensation: job.compensation,
-        })),
-        total: response.data.total,
-      })
     }
 
-    req();
+    setState({
+      ...state,
+      jobs: response.data.data.map(job => ({
+        id: job.id,
+        title: job.objective,
+        organization: job.organizations,
+        skills: job.skills,
+        type: job.type,
+        status: job.status,
+        deadline: job.deadline,
+        locations: job.locations,
+        compensation: job.compensation,
+      })),
+      total: response.data.total,
+    })
+  }
+
+  useEffect(() => {
+    search();
     window.scrollTo(0, 0)
   }, [state.size, state.actualPage])
 
@@ -251,13 +207,8 @@ const Jobs = () => {
         }
       </div>
       <div className="pages">
-        <span onClick={goTo}>First</span>
-        {
-          pages().map(page => (
-            <p onClick={updatePage}>{page}</p>
-          ))
-        }
-        <span onClick={goTo}>Last</span>
+        <button type="button" onClick={goTo} disabled={state.actualPage === 1}>Prev</button>
+        <button type="button" onClick={goTo} disabled={state.actualPage === Math.ceil(state.total / state.size)}>Next</button>
       </div>
     </div>
   );
--- src/containers/Professionals/index.jsx
+++ src/containers/Professionals/index.jsx
@@ -14,33 +14,6 @@ const Professionals = () => {
 
   const [formData, setFormData] = useState({});
 
-  const pages = () => {
-    const { actualPage, size, total } = state;
-    const lastPage = total % size === 0 ? total / size : Math.ceil(total / size);
-    const menuLength = 7;
-
-    if (actualPage < 5) {
-      return lastPage < menuLength
-        ? Array.from({ length: lastPage }, (x, i) => i + 1)
-        : Array.from({ length: menuLength }, (x, i) => i + 1)
-    }
-    if (actualPage > lastPage - 4) {
-      return lastPage < menuLength
-        ? Array.from({ length: lastPage }, (x, i) => i + 1)
-        : Array.from({ length: menuLength }, (x, i) => lastPage - 6 + i)
-    }
-    if (actualPage > 4 && actualPage < lastPage - 3) {
-      return Array.from({ length: menuLength }, (x, i) => actualPage - 3 + i)
-    }
-  }
-
-  const updatePage = e => {
-    setState({
-      ...state,
-      actualPage: e.target.innerHTML,
-    })
-  }
-
   const updateSize = e => {
     setState({
       ...state,
@@ -49,21 +22,25 @@ const Professionals = () => {
   }
 
   const goTo = e => {
-    const { size, total } = state;
-    const lastPage = total % size === 0 ? total / size : Math.ceil(total / size);
-    if (e.target.innerHTML === 'First') {
+    let newPage = state.actualPage;
+    if (e.target.innerHTML === 'Prev') {
+      newPage--;
       setState({
         ...state,
-        actualPage: 1,
+        actualPage: newPage,
       });
     }
-    if (e.target.innerHTML === 'Last') {
+    if (e.target.innerHTML === 'Next') {
+      newPage++
       setState({
         ...state,
-        actualPage: lastPage,
+        actualPage: newPage,
       });
     }
+    console.log(state.actualPage);
+    console.log(state.actualPage === Math.ceil(state.total / state.size));
   }
+
 
   const clearField = e => {
     const radioButtons = e.target.parentElement.children;
@@ -73,16 +50,6 @@ const Professionals = () => {
     }
     delete formData[radioButtons[1].name]
   };
-
-  const clearText = e => {
-    const textField = e.target.parentElement.children;
-
-    for (let i = 1; i < textField.length; i += 2) {
-      textField[i].value = '';
-    }
-
-    delete formData[textField.name];
-  }
 
   const handleChange = e => {
     const element = e.target;
@@ -104,60 +71,42 @@ const Professionals = () => {
   }
 
   const search = async e => {
-    e.preventDefault();
+    e && e.preventDefault();
     const { actualPage, size } = state;
     const offset = (actualPage - 1) * size;
     const hasData = Object.keys(formData).length > 0;
+    let response;
 
     if (hasData) {
-      const response = await axios({
+      response = await axios({
         method: 'post',
         url: `/people/search?size=${size}&offset=${offset}`,
         data: formData,
       });
-
-      setState({
-        ...state,
-        professionals: response.data.data.map(professional => ({
-          username: professional.username,
-          location: professional.locationName,
-          name: professional.name,
-          openTo: professional.openTo,
-          picture: professional.picture,
-          skills: professional.skills,
-          professionalHeadline: professional.professionalHeadline,
-        })),
-        total: response.data.total,
-      })
-    }
-  }
-
-  useEffect(() => {
-    const req = async () => {
-      const { actualPage, size } = state;
-      const offset = (actualPage - 1) * size
-
-      const response = await axios({
+    } else {
+      response = await axios({
         method: 'post',
         url: `/people/search?size=${size}&offset=${offset}`,
       });
-
-      setState({
-        ...state,
-        professionals: response.data.data.map(professional => ({
-          username: professional.username,
-          location: professional.locationName,
-          name: professional.name,
-          openTo: professional.openTo,
-          picture: professional.picture,
-          skills: professional.skills,
-          professionalHeadline: professional.professionalHeadline,
-        })),
-        total: response.data.total,
-      });
     }
 
-    req();
+    setState({
+      ...state,
+      professionals: response.data.data.map(professional => ({
+        username: professional.username,
+        location: professional.locationName,
+        name: professional.name,
+        openTo: professional.openTo,
+        picture: professional.picture,
+        skills: professional.skills,
+        professionalHeadline: professional.professionalHeadline,
+      })),
+      total: response.data.total,
+    })
+  }
+
+  useEffect(() => {
+    search();
     window.scrollTo(0, 0)
   }, [state.size, state.actualPage])
 
@@ -209,13 +158,8 @@ const Professionals = () => {
         }
       </div>
       <div className="pages">
-        <span onClick={goTo}>First</span>
-        {
-          pages().map(page => (
-            <p onClick={updatePage}>{page}</p>
-          ))
-        }
-        <span onClick={goTo}>Last</span>
+        <button type="button" onClick={goTo} disabled={state.actualPage === 1}>Prev</button>
+        <button type="button" onClick={goTo} disabled={state.actualPage === Math.ceil(state.total / state.size)}>Next</button>
       </div>
     </div>
   );
